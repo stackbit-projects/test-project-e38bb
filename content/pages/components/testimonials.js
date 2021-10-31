41d78bb752f3253207610fe4fbd70516e2ff2de8
Stackbit Code Editor <projects@stackbit.com>
2021-10-31 06:50:39 +0000

    testimonials.js: changed

--- content/pages/components/testimonials.js
+++ content/pages/components/testimonials.js
@@ -2,7 +2,7 @@ const TestimonialsSection = (props) => {
   const { annotationPrefix, testimonials, title } = props;
   return (
     <div
-      className="max-w-screen-xl mx-auto px-4 sm:px-6 py-14 lg:py-20 mt-10 mb-10 text-center"
+      className=""
       data-sb-field-path={annotationPrefix}>
       <h1
         className="text-3xl tracking-tight sm:text-4xl mb-2"
