dcd0994206f5806243da716bde005b0283acf982
Stackbit Code Editor <projects@stackbit.com>
2021-10-31 06:57:41 +0000

    register-components.js: changed

--- src/components/register-components.js
+++ src/components/register-components.js
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 import { registerComponents } from '@stackbit/components';
 import { componentsMap } from '@stackbit/components/dist/components-map';
-import Testimonials from './testimonials'
+import TestimonialsSec from './testimonials'
 
 registerComponents({
     // Register all Stackbit components
