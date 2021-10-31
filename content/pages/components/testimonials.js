5245a37bd3c71b0adeb8afa72b28ddee1bf7ef5f
Stackbit Code Editor <projects@stackbit.com>
2021-10-31 06:54:38 +0000

    testimonials.js: changed

--- content/pages/components/testimonials.js
+++ content/pages/components/testimonials.js
@@ -1,38 +1,31 @@
 const TestimonialsSection = (props) => {
-  const { annotationPrefix, testimonials, title } = props;
-  return (
-    <div
-      className=""
-      data-sb-field-path={annotationPrefix}>
-      <h1
-        className=""
-        data-sb-field-path=".title">
-        {title}
-      </h1>
-      <div
-        className=""
-        data-sb-field-path=".logos">
-        {testimonials.map((testimonial, index) => (
-          <div className="" key={index} data-sb-field-path={`.${index}`}>
-            <img
-              className=""
-              height="60px"
-              width="60px"
-              src={logo.image}
-              data-sb-field-path=".image#@src"
-            />
-            <div data-sb-field-path=".mainContent"> 
-                {testimonial.mainContent}
+    const { annotationPrefix, testimonials, title } = props;
+    return (
+        <div
+            className=""
+            data-sb-field-path={annotationPrefix}>
+            <h1
+                className=""
+                data-sb-field-path=".title">
+                {title}
+            </h1>
+            <div
+                className=""
+                data-sb-field-path=".logos">
+                {testimonials.map((testimonial, index) => (
+                    <div className="" key={index} data-sb-field-path={`.${index}`}>
+                        <div data-sb-field-path=".mainContent">
+                            {testimonial.mainContent}
+                        </div>
+
+                        <div data-sb-field-path=".author">
+                            {testimonial.author}
+                        </div>
+                    </div>
+                ))}
             </div>
-            
-            <div data-sb-field-path=".author">
-              {testimonial.author}
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default TestimonialsSection;
